bf274ffa1085fe900baab9dc8d3d8da2acca63a1
NoelieAB <51463568+NoelieAB@users.noreply.github.com>
2019-06-17 14:19:32 -0500

    Add files via upload
    Reads excel file into R as a nibble

--- Read Data.R
+++ Read Data.R
@@ -6,9 +6,9 @@ require(readxl)
 # Read in the raw data not using the row names
 # because they contain non-ascii characters
 # Assign NA to all relevant strings
-read_excel("Survey 6:17:19.xlsx", 
-           col_names = FALSE,
-           na = c("N/A", "n/a", "NA", "na", "Na", "N/a", "Not applicable", "",
-                  "Not applicable; my family member does not attend a Down syndrome specialty clinic",
-                  "Not sure why this came up and I can't go back and see what answer this is referring to.") ) -> 
+as_tibble(read_excel("Survey 6:17:19.xlsx", 
+                     col_names = FALSE,
+                     na = c("N/A", "n/a", "NA", "na", "Na", "N/a", "Not applicable", "",
+                            "Not applicable; my family member does not attend a Down syndrome specialty clinic",
+                            "Not sure why this came up and I can't go back and see what answer this is referring to."))) -> 
   DSSurveyraw
